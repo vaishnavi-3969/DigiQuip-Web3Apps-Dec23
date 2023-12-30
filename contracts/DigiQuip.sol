3827e306c41816c8793cc7d8927b367603bd90c2
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-12-30 16:41:18 +0530

    Add minting functionality and event handling

--- contracts/DigiQuip.sol
+++ contracts/DigiQuip.sol
@@ -3,8 +3,54 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract DigiQuip is ERC721URIStorage {
+    uint256 public tokenCount;
+    uint256 public postCount;
+
+    mapping(uint256 => Post) public posts;
+    //address => nft id
+    mapping(address => uint256) public profiles;
+
+    struct Post {
+        uint256 id;
+        string hash;
+        // string content;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event PostCreated (
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event PostTipped (
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable author
+    );
+
     constructor() ERC721("DigiQuip", "DAPP") {}
+
+    function mint(string memory _tokenURI) external returns (uint256){
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return tokenCount;
+    }
 }
+
+
+
+
+
+
+
+
+
--- test/DigiQuip.test.js
+++ test/DigiQuip.test.js
@@ -2,5 +2,36 @@ const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
 describe("DigiQuip", function () {
+    let digiquip
+    let deployer, signer1, signer2, signers
+    let URI = "DemoURI"
+    let postHash = "DemoHash"
+    beforeEach(async () => {
+        [deployer, signer1, signer2, ...signers] = await ethers.getSigners()
+        const DigiQuip = await ethers.getContractFactory("DigiQuip")
+        digiquip = await DigiQuip.deploy()
+        //signer1 mints an nft
+        await digiquip.connect(signer1).mint(URI)
+    })
+    describe("Deployment", async () => {
+        it("Should set the right owner", async function () {
+            it("Should track name and symbol", async function () {
+                const nftName = "DigiQuip"
+                const nftSymbol = "DAPP"
+                expect(await digiquip.name()).to.equal(nftName)
+                expect(await digiquip.symbol()).to.equal(nftSymbol)
+            });
+        })
+    });
 
-})
+
+
+
+
+
+
+
+
+
+
+

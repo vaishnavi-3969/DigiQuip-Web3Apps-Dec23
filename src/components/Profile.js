9a6c589e63c8a23adcdcb0f77cf9579b1da12d70
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-12-31 14:30:34 +0530

    Update contract address and add account prop to Home component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,7 +13,7 @@ const Profile = ({ contract }) => {
     const [loading, setLoading] = useState(true)
     const loadMyNFTs = async () => {
         // Get users nft ids
-        const results = await contract.getAllNFTs();
+        const results = await contract.getAllNfts();
         // Fetch metadata of each nft and add that to nft object.
         let nfts = await Promise.all(results.map(async i => {
             // get uri url of nft

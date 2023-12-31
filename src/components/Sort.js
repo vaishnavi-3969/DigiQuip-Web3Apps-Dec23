9bee935ad218f810ffb57adb4b9f4cf2ffb56fb3
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-12-31 17:57:37 +0530

    Refactor Sort component to use react-bootstrap Dropdown

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,22 +1,46 @@
-import down from '../assets/angle-down-solid.svg';
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
 
 const Sort = () => {
   return (
     <div className="sort bg-gradient-to-r from-[#12e0b7] via-[#a6c0fe] to-[#fbcfe8] p-6 rounded-md shadow-md">
-      <div className="sort__select">
-        <p className="text-lg font-semibold text-gray-800">Select Category</p>
-        <img src={down} alt="Dropdown" className="ml-2 h-4 w-4" />
-      </div>
+      <Dropdown className="sort__select">
+        <Dropdown.Toggle variant="light" id="categoryDropdown">
+          Select Category
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#music">Music</Dropdown.Item>
+          <Dropdown.Item href="#tech">Sports</Dropdown.Item>
+          <Dropdown.Item href="#sports">Tech</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
 
-      <div className="sort__select mt-4">
-        <p className="text-lg font-semibold text-gray-800">Select Your Dates</p>
-        <img src={down} alt="Dropdown" className="ml-2 h-4 w-4" />
-      </div>
+      <Dropdown className="sort__select mt-4">
+        <Dropdown.Toggle variant="light" id="datesDropdown">
+          Select Your Dates
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="/">01/01/2024</Dropdown.Item>
+          <Dropdown.Item href="/">02/01/2024</Dropdown.Item>
+          <Dropdown.Item href="/">03/01/2024</Dropdown.Item>
+          <Dropdown.Item href="/">04/01/2024</Dropdown.Item>
+          <Dropdown.Item href="/">05/01/2024</Dropdown.Item>
+          <Dropdown.Item href="/">06/01/2024</Dropdown.Item>
+          <Dropdown.Item href="/">07/01/2024</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
 
-      <div className="sort__select mt-4">
-        <p className="text-lg font-semibold text-gray-800">Select Your Distance</p>
-        <img src={down} alt="Dropdown" className="ml-2 h-4 w-4" />
-      </div>
+      <Dropdown className="sort__select mt-4">
+        <Dropdown.Toggle variant="light" id="distanceDropdown">
+          Select Your Distance
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="/">Upto 5 kms</Dropdown.Item>
+          <Dropdown.Item href="/">Upto 30 kms</Dropdown.Item>
+          <Dropdown.Item href="/">Upto 50 kms</Dropdown.Item>
+          <Dropdown.Item href="/">Greater than 50 kms</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 };
